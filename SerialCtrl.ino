72f5a062c24e8d950773433d16f19e7058ee3f2a
dajiao <mck_dajiao@outlook.com>
2013-11-19 14:06:23 +0800

    adding a describtion to protocol

--- SerialCtrl.ino
+++ SerialCtrl.ino
@@ -6,24 +6,27 @@ now for mega only
 
 protocol:
 	^[1][2][3][]...[]$
-[1] : 0 for operate a digital pin 
-	  1 for operate a analog pin
-	  2 for using a SPI	
-	  3 
+[1] :	0 for operate a digital pin 
+		1 for operate a analog pin
+[2] : 	0 for read
+	  	1 for write(only support for a digital pin)
+[3] :	depend on [1][2]	
+		case 00:	$ return format (^00[1]$) which [1] equals the value
+		case 01:	0 for normal write(0/1) [4] equals the value to write end with $
+					1 for PWM write [4][5] equals the value to write end with $
+		case 10:	$ return format (^10[1][2]$) which [1][2] equals the value	
+		case 11:	not support write to a analog pin	
 */
 
 void setup(){
 	// init the Serial port 
 	Serial.begin(9600);
 	Serial1.begin(9600);
-	Serial2.begin(9600);
-	Serial3.begin(9600);
-
 }
 
 void loop(){
 	if(Serial1.available()>0){
-			
+				
 	}
 }
 
